0869bb19af251af0b8d34780e94655faadc304fd
Yegor Bugayenko <yegor256@gmail.com>
2025-05-07 20:12:23 +0300

    typos

--- 09-model-checking/jpf/Race.java
+++ 09-model-checking/jpf/Race.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Yegor Bugayenko
+// SPDX-License-Identifier: MIT
+
 public class Race {
   static int d = 42;
   public static void main (String[] args) throws Exception {
